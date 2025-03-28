ea54f7a6ecbc07ad332b8544b613c2253bd77e8a
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-03-28 17:09:29 +0500

    continue at add task

--- todo.py
+++ todo.py
@@ -3,7 +3,7 @@ import json  # built in module in python  -- to save and load the task file
 import os  # built in module in python   -- operating system -- use to check file exist or not
 
 TODO_FILE = "todo.json"  # this variable hold our file
-
+ 
 
 # function
 def load_task():  # load task from todo.json
@@ -12,7 +12,7 @@ def load_task():  # load task from todo.json
     with open(TODO_FILE, "r") as file:
         # r is used for read mode and extract data in read mode
         #  #with is used to manage resources -- data manage
-        return json.load(file)
+        return json.load(file) # load data from file
 
 
 def save_task(tasks):  # user defined task
@@ -20,3 +20,17 @@ def save_task(tasks):  # user defined task
         json.dump(
             tasks, file, indent=4
         )  # store data in todo json file # user defined taske # indentation for json file
+
+@click.group()
+def cli():
+    """Simple todo list manager"""
+    pass
+
+@click.command()
+@click.argument("task") # argument provided by us
+def add (task):
+    """Add new task into list"""
+    tasks = load_task()  # open file
+    tasks.append({ "task" : task ,  "done" : False}) # store a dictionary in a file this task variable
+    save_task(tasks)
+    click.echo(f"Task added successfully: {task}") # this task is coming from @click.argument ("task")
